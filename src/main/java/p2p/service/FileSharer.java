d96815ced905799c7b2e639c01d0516b0008dcf8
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-01-31 14:16:30 +0530

    MapForfileAvailibilty

--- src/main/java/p2p/service/FileSharer.java
+++ src/main/java/p2p/service/FileSharer.java
@@ -1,4 +1,15 @@
 package p2p.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
 public class FileSharer {
+    private HashMap<Integer , String> availableFiles;
+
+    public FileSharer(){
+        availableFiles = new HashMap<>();
+    }
+
 }
