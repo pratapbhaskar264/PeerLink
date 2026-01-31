c93194e303634debcb95ae4d7213f6ecbdc29a88
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-01-31 14:45:18 +0530

    file-availibilityToPort

--- src/main/java/p2p/service/FileSharer.java
+++ src/main/java/p2p/service/FileSharer.java
@@ -13,12 +13,14 @@ public class FileSharer {
         availableFiles = new HashMap<>(); // this will store files available at certain port as of now
     }
 
-    private int offerFile(String filePath) {
+    private int offerFile(String filePath) { // it is taking file path and returning port ->
+        // -> after its loaded with file in available file map
         int port;
         while(true) {
            port = UploadUtils.genrateCode();
            if(!availableFiles.containsKey(port)) {
                availableFiles.put(port,filePath);
+               return port;
            }
         }
     }
