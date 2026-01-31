ba6fc263d0029bfe736bf8fc7af897d9d2adb887
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-01-31 14:55:38 +0530

    download-handler

--- src/main/java/p2p/service/FileSharer.java
+++ src/main/java/p2p/service/FileSharer.java
@@ -3,6 +3,8 @@ package p2p.service;
 import org.springframework.stereotype.Service;
 import p2p.utility.UploadUtils;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.HashMap;
 
 @Service
@@ -13,6 +15,7 @@ public class FileSharer {
         availableFiles = new HashMap<>(); // this will store files available at certain port as of now
     }
 
+    // uploading end
     private int offerFile(String filePath) { // it is taking file path and returning port ->
         // -> after its loaded with file in available file map
         int port;
@@ -23,6 +26,21 @@ public class FileSharer {
                return port;
            }
         }
+    }
+
+    //downloading end
+    public void startFileSharer(int port) {
+        if(!availableFiles.containsKey(port)) {
+            System.out.println("no such file associated with port " +port);
+            return;
+        }
+        String filePath = availableFiles.get(port);
+        try(ServerSocket serverSocket = new ServerSocket(port) ) {
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
