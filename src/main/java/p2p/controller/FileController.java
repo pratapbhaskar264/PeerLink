f8a3778457f560c7dd8155fb1545eb2a1039f6af
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-02-04 14:19:48 +0530

    Controller

--- src/main/java/p2p/controller/FileController.java
+++ src/main/java/p2p/controller/FileController.java
@@ -1,14 +1,21 @@
 package p2p.controller;
 
+import com.sun.net.httpserver.HttpServer;
 import org.springframework.web.bind.annotation.RestController;
 import p2p.service.FileSharer;
 
 @RestController
 public class FileController {
     private final FileSharer fileSharer;
+    public final HttpServer httpServer;
+    private final String uploadDir;
+    private final ExcecutorService excecutorService;
 
-    public FileController(FileSharer fileSharer) {
+    public FileController(FileSharer fileSharer, HttpServer httpServer, String uploadDir, ExcecutorService excecutorService) {
         this.fileSharer = fileSharer;
+        this.httpServer = httpServer;
+        this.uploadDir = uploadDir;
+        this.excecutorService = excecutorService;
     }
 
 }
