224d287bb05b188f54b5775733ac346c521f348b
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-02-04 14:13:53 +0530

    Controller

--- src/main/java/p2p/controller/FileController.java
+++ src/main/java/p2p/controller/FileController.java
@@ -1,4 +1,14 @@
 package p2p.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import p2p.service.FileSharer;
+
+@RestController
 public class FileController {
+    private final FileSharer fileSharer;
+
+    public FileController(FileSharer fileSharer) {
+        this.fileSharer = fileSharer;
+    }
+
 }
