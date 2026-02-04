4938f88d7a6fb7afce69960c906ca363d7c7408b
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-02-04 14:28:38 +0530

    Controller

--- src/main/java/p2p/controller/FileController.java
+++ src/main/java/p2p/controller/FileController.java
@@ -4,18 +4,27 @@ import com.sun.net.httpserver.HttpServer;
 import org.springframework.web.bind.annotation.RestController;
 import p2p.service.FileSharer;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @RestController
 public class FileController {
     private final FileSharer fileSharer;
     public final HttpServer httpServer;
+    //temp dir which will store files on temp basis
     private final String uploadDir;
-    private final ExcecutorService excecutorService;
+    private final ExecutorService excecutorService;
 
-    public FileController(FileSharer fileSharer, HttpServer httpServer, String uploadDir, ExcecutorService excecutorService) {
-        this.fileSharer = fileSharer;
-        this.httpServer = httpServer;
-        this.uploadDir = uploadDir;
-        this.excecutorService = excecutorService;
+    public FileController( int port) throws IOException {
+        this.fileSharer = new FileSharer();
+        this.httpServer = HttpServer.create(new InetSocketAddress(port) , 0);
+
+        this.uploadDir = System.getProperty("java.io.tempdir") + File.separator + "peerlink-uploads";
+        this.excecutorService = Executors.newFixedThreadPool(10);
     }
 
 }
