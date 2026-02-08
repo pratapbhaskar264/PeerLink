d2e52972253c85fcdfb88af18a85a43994461d3c
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-02-08 16:45:13 +0530

    Main-Method-Aesthetics

--- src/main/java/p2p/App.java
+++ src/main/java/p2p/App.java
@@ -3,9 +3,8 @@ package p2p;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+
     }
 }
