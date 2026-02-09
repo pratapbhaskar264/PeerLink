b346d2d8ff25e59d5d7b9c4e022ed6fcba7a65d5
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-02-09 10:12:18 +0530

    Main-Method-Aesthetics

--- src/main/java/p2p/App.java
+++ src/main/java/p2p/App.java
@@ -12,6 +12,14 @@ public class App {
           fileController.start();
           System.out.println("Peerlink started at port 8080");
           System.out.println("UI available at http://localhost:3000");
+          Runtime.getRuntime().addShutdownHook( new Thread( () -> {
+              System.out.println("Shutting down the server");
+              fileController.stop();
+                  })
+          );
+          System.out.println("Press enter to stop the server");
+          System.in.read(); //homework to stop the server when someone presses the enter key
+
 
       } catch (IOException e) {
           System.err.println("Failed to start server at port 8080");
