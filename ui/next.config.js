cd11c86e91d7f3c756a3df30e24792e3589ced2d
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-04-18 23:06:05 +0530

    Move rewrites to vercel.json

--- ui/next.config.js
+++ ui/next.config.js
@@ -2,18 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/upload',
-        destination: 'https://peerlink-hst3.onrender.com/upload',
-      },
-      {
-        source: '/api/download/:port',
-        destination: 'https://peerlink-hst3.onrender.com/download/:port',
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
